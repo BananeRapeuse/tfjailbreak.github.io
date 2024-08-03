fc60bf28110b24e7b1c5c9e29f28369667b6501e
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-03 23:55:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.getElementById('jailbreak-button').addEventListener('click', function()
     let progressBar = document.getElementById('progress-bar');
     let progressContainer = document.getElementById('progress-container');
     progressContainer.style.display = 'block';
-    let width = 0;
+    let height = 0;
     let interval = setInterval(function() {
-        width += 25;
-        progressBar.style.width = width + '%';
-        if (width >= 100) {
+        height += 25;
+        progressBar.style.height = height + '%';
+        if (height >= 100) {
             clearInterval(interval);
         }
     }, 2000);
